c5ab78aa24b7119008731b893f5618f2caf32d52
Andre Schevz de Werk <andrewerk1804@gmail.com>
2022-01-07 18:44:34 -0300

    First Requirement

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,23 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  
+// Primeira solução desenvolvida:
+// function getSpeciesByIds(...ids) {
+//   const list = [];
+//   ids.forEach((id) => {
+//     const filtered = species.filter((specie) => specie.id === id);
+//     list.push(filtered);
+//   });
+//   return list;
+// }
+
+// segunda solução desenvolvida, com o apoio do Ronald em Mentoria
+function getSpeciesByIds(...ids) {
+  const list = ids.map((id) => {
+    const filtered = species.find((specie) => specie.id === id);
+    return filtered;
+  });
+  return list;
 }
 
 module.exports = getSpeciesByIds;
