65d2fc058dfb9d21d5c357287bbddc84c1bcd3a8
Andre Schevz de Werk <andrewerk1804@gmail.com>
2022-01-09 19:03:48 -0300

    Third requirement

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,15 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  let result = {};
+  if (!employeeName) {
+    return result;
+  }
+  const searchName = employees.find((employee) => employee.firstName === employeeName);
+  const searchLastName = employees.find((employee) => employee.lastName === employeeName);
+  result = (searchName === undefined) ? result = searchLastName : result = searchName;
+  return result;
 }
 
 module.exports = getEmployeeByName;
