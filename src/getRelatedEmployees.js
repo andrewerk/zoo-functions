5345f3cd121f6ce1f83d6eeb3786f78a443a71fd
Andre Schevz de Werk <andrewerk1804@gmail.com>
2022-01-09 20:01:39 -0300

    Fourth requirement

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,24 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  const verifyManager = employees.some((employee) => employee.managers.includes(id));
+  return verifyManager;
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  } else {
+    const managed = [];
+    employees.forEach((employee) => {
+      if (employee.managers.includes(managerId)) {
+        managed.push(`${employee.firstName} ${employee.lastName}`);
+      }
+    });
+    return managed;
+  }
 }
 
+console.log(getRelatedEmployees('b0dc644a-5335-489b-8a2c-4e086c7819a2'));
 module.exports = { isManager, getRelatedEmployees };
