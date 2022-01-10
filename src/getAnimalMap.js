20fcf26f9b70ee9eb350d6a23f7e39db42379d53
Andre Schevz de Werk <andrewerk1804@gmail.com>
2022-01-10 00:18:02 -0300

    Ninth requirement

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,32 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+const northEast = [];
+const northWest = [];
+const southEast = [];
+const southWest = [];
+
+const result = {
+  NE: northEast, NW: northWest, SE: southEast, SW: southWest,
+};
+
+function noParametersGiven() {
+  species.forEach((specie) => {
+    if (specie.location === 'NE') { northEast.push(specie.name); }
+    if (specie.location === 'NW') { northWest.push(specie.name); }
+    if (specie.location === 'SE') { southEast.push(specie.name); }
+    if (specie.location === 'SW') { southWest.push(specie.name); }
+  });
+  return result;
+}
+
 function getAnimalMap(options) {
-  // seu código aqui
+  if (!options) {
+    return noParametersGiven();
+  }
+  if (options.sex === 'male' || options.sex === 'female') {
+    return 
+  }
 }
 
 module.exports = getAnimalMap;
--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,13 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const funcionario = employees.find((employee) => employee.id === id);
+  const firstSpecie = funcionario.responsibleFor[0];
+  const animal = species.find((specie) => specie.id === firstSpecie);
+  const callBack = (acc, elem) => (elem.age > acc.age ? elem : acc);
+  const oldestAnimal = animal.residents.reduce(callBack);
+  return Object.values(oldestAnimal);
 }
 
 module.exports = getOldestFromFirstSpecies;
