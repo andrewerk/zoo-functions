db6df20bad1463292ae049d60ea876966cace628
Andre Schevz de Werk <andrewerk1804@gmail.com>
2022-01-10 09:16:35 -0300

    Lint Corrections

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,32 +1,33 @@
 const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-const northEast = [];
-const northWest = [];
-const southEast = [];
-const southWest = [];
+console.log(species);
+// const northEast = [];
+// const northWest = [];
+// const southEast = [];
+// const southWest = [];
 
-const result = {
-  NE: northEast, NW: northWest, SE: southEast, SW: southWest,
-};
+// const result = {
+//   NE: northEast, NW: northWest, SE: southEast, SW: southWest,
+// };
 
-function noParametersGiven() {
-  species.forEach((specie) => {
-    if (specie.location === 'NE') { northEast.push(specie.name); }
-    if (specie.location === 'NW') { northWest.push(specie.name); }
-    if (specie.location === 'SE') { southEast.push(specie.name); }
-    if (specie.location === 'SW') { southWest.push(specie.name); }
-  });
-  return result;
-}
+// function noParametersGiven() {
+//   species.forEach((specie) => {
+//     if (specie.location === 'NE') { northEast.push(specie.name); }
+//     if (specie.location === 'NW') { northWest.push(specie.name); }
+//     if (specie.location === 'SE') { southEast.push(specie.name); }
+//     if (specie.location === 'SW') { southWest.push(specie.name); }
+//   });
+//   return result;
+// }
 
 function getAnimalMap(options) {
-  if (!options) {
-    return noParametersGiven();
-  }
-  if (options.sex === 'male' || options.sex === 'female') {
-    return 
-  }
+//   if (!options) {
+//     return noParametersGiven();
+//   }
+//   if (options.sex === 'male' || options.sex === 'female') {
+//     return
+//   }
 }
 
 module.exports = getAnimalMap;
