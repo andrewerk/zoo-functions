147cdac4e28591dc546173dba6e8bcb9f81ee4f0
Andre Schevz de Werk <andrewerk1804@gmail.com>
2022-01-10 17:29:48 -0300

    Requirement 8

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,38 @@
+const { hours, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+function noValidParameter() {
+  const schedule = {};
+  Object.entries(hours).forEach((day) => {
+    const exhib = species.filter((sp) => sp.availability.includes(day[0])).map((sp) => sp.name);
+    let result = {
+      [day[0]]: {
+        officeHour: `Open from ${day[1].open}am until ${day[1].close}pm`,
+        exhibition: exhib,
+      },
+    };
+    if (Object.values(day)[0] === 'Monday') {
+      result = { Monday: { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' },
+      };
+    }
+    Object.assign(schedule, result);
+  });
+  return schedule;
+}
+
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  const noParameter = noValidParameter();
+  if (noParameter[scheduleTarget]) {
+    const result = {
+      [scheduleTarget]: noParameter[scheduleTarget],
+    };
+    return result;
+  }
+  const animalGiven = species.find((specie) => specie.name === scheduleTarget);
+  if (animalGiven) {
+    return animalGiven.availability;
+  }
+  return noParameter;
 }
 
 module.exports = getSchedule;
